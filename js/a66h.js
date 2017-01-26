b18237dad32cd3b5e94f0ae600b6270a1a90f3c1
Fejes Aniko <oranjje@gmail.com>
2017-01-26 19:23:15 +0100

    feat(datasources/merge): merge 2 data sources by date

--- js/a66h.js
+++ js/a66h.js
@@ -1,1 +1,7 @@
-render(mockDataSource.get(), document.querySelector('#timeline'))
+let flattenArrays = (arrays) =>  [].concat.apply([], arrays)
+let sortByDate = (item1, item2) => item1.date - item2.date
+
+Promise.all([mockDataSource.get(), mockDataSource2.get()]).then(timelineArrays => {
+  timelineItems = flattenArrays(timelineArrays).sort(sortByDate);
+  render(timelineItems, document.querySelector('#timeline'))
+});
--- js/datasource.js
+++ js/datasource.js
@@ -1,31 +1,64 @@
+const mockDate = Date.now()
+
 const mockDataSource = (() => {
   const mockData = [
     {
-      date: new Date(),
+      date: mockDate,
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
       type: 'mock',
       team: 'Lone fox',
     },
     {
-      date: new Date(),
+      date: mockDate + 10,
       text: 'eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       type: 'mock',
       team: 'Lone fox',
     },
     {
-      date: new Date(),
+      date: mockDate + 20,
       text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
       type: 'mock',
       team: 'Lone fox',
     },
     {
-      date: new Date(),
+      date: mockDate + 30,
       text: ' nisi ut aliquip ex ea commodo consequat.',
       type: 'mock',
       team: 'Lone fox',
     },
   ]
   return {
-    get: () => mockData
+    get: () => new Promise((resolve) => resolve(mockData))
+  }
+})()
+const mockDataSource2 = (() => {
+  const mockData = [
+    {
+      date: mockDate + 5,
+      text: '2 Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+      type: 'mock',
+      team: 'Lone fox',
+    },
+    {
+      date: mockDate + 100,
+      text: '2 eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      type: 'mock',
+      team: 'Lone fox',
+    },
+    {
+      date: mockDate + 15,
+      text: '2 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+      type: 'mock',
+      team: 'Lone fox',
+    },
+    {
+      date: mockDate + 1,
+      text: '2  nisi ut aliquip ex ea commodo consequat.',
+      type: 'mock',
+      team: 'Lone fox',
+    },
+  ]
+  return {
+    get: () =>  new Promise((resolve) => resolve(mockData))
   }
 })()

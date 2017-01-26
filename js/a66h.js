26d2b9513eb1e85331521e95653512ff671f76a9
Fejes Aniko <oranjje@gmail.com>
2017-01-26 20:53:58 +0100

    feat(datasrc/slack): add a66 slack:fedex-day datasource

--- js/a66h.js
+++ js/a66h.js
@@ -1,7 +1,12 @@
+const dataSources = [
+  mockDataSource,
+  slackDataSource
+]
+
 let flattenArrays = (arrays) =>  [].concat.apply([], arrays)
 let sortByDate = (item1, item2) => item1.date - item2.date
 
-Promise.all([mockDataSource.get(), mockDataSource2.get()]).then(timelineArrays => {
+Promise.all(dataSources.map(src => src.get())).then(timelineArrays => {
   timelineItems = flattenArrays(timelineArrays).sort(sortByDate);
   render(timelineItems, document.querySelector('#timeline'))
 });
---
+++ js/slackDataSource.js
@@ -0,0 +1,19 @@
+const slackDataSource = (() => {
+  const SHEETID = '1zyx2ey7RdcubMEhAUXJA_PrqED9TQlhlTlUy_MxUbvM'
+  const APIKEY = 'AIzaSyA5BRzp7GK1DDXciqpRXuK0v8mJanTMM1w'
+  const RANGE = 'A2:D'
+  return {
+    get: () => fetch(
+          `https://sheets.googleapis.com/v4/spreadsheets/${SHEETID}/values/${RANGE}?key=${APIKEY}`,
+          { method: 'get' }
+        )
+        .then(response => response.json())
+        .then(responseJSON => responseJSON.values.map((row) => ({
+          date: row[0],
+          text: `[slack] ${row[3]}`,
+          author: row[2],
+          type: 'slack',
+          team: 'Lone Wolfs',
+        })))
+  }
+})()
